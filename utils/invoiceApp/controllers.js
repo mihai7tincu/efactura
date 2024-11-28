510a0bfecf9a6566d93e32fd760784f955d53b71
Mihai Tincu <mihai7tincu@gmail.com>
2024-11-28 23:16:25 +0200

    F26

--- utils/invoiceApp/controllers.js
+++ utils/invoiceApp/controllers.js
@@ -8,8 +8,8 @@
 
         $scope.title = 'Calcule factura';
         $scope.exchangeRate = 4.9759;
-        $scope.rate = 42;
-        $scope.vatPercentage = 0;
+        $scope.rate = 40;
+        $scope.vatPercentage = 19;
         $scope.workedHours = 218;
         $scope.getUnitPrice = function () {
             if (!$scope.rate)
